261decbe56fd26a258f84a82fdaf64ac43ac1ce2
Pradumn Singh <pradumnkumar0012@gmail.com>
2022-06-30 18:25:40 +0530

    adding Java program

---
+++ sequence.java
@@ -0,0 +1,8 @@
+class sequence{
+    public static void main(String args[]) {
+    int n = 100;
+    System. out. print("Numbers from 1 to "+n+" are: ");
+    for (int i = 1; i <= n; i++) {
+    System. out. print(i + " ");
+    }
+    }}
