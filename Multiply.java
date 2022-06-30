506dc82340c7ffa426d446fdb0123bd2edccf1ad
Pradumn Singh <pradumnkumar0012@gmail.com>
2022-06-30 18:56:14 +0530

    adding Multiply Program

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String args[]) {
+        int n1 = 12;
+        int n2 = 45;
+        int t= n1*n2;
+        System.out.println(t);
+        
+    }
+}
